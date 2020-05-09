abe644751da66b51583f52929c59098f0b0122e8
Sarthak Mehrish <sarthakmeh03@gmail.com>
2020-05-09 23:28:09 +0530

    [BugFix] Serializer bugfix

--- app/serializers.py
+++ app/serializers.py
@@ -14,7 +14,7 @@ class SessionSerializer(serializers.Serializer):
     status = serializers.CharField(max_length=20, default='NEW')
     is_accepted = serializers.BooleanField()
     is_disputed = serializers.BooleanField()
-    disputed_message = serializers.TextField()
+    disputed_message = serializers.CharField(max_length=240)
 
 
 class AvailabilitySerializer(serializers.Serializer):
